ed881107f9ee1be54db7edd98900d3366b701fc1
Ryan Dick <rdick@ualberta.ca>
2021-09-16 00:28:50 -0600

    finished coding of nasa shopify project - now to comment and simplify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import { Container, Spinner } from 'reactstrap';
+import { Button, Container, FormGroup, Input, Label, Spinner } from 'reactstrap';
 import { useState } from 'react';
 import PictureCard from './components/PictureCard';
+import { ToastProvider } from 'react-toast-notifications';
 
 const key = '1QyMmRUtp3Mfb47CNUWtJmhIUC4cORz8TqlCYM6g';
 
 function App() {
 	const [startDate, setStartDate] = useState(`2017-08-10`);
 	const [endDate, setEndDate] = useState(`2017-08-20`);
+	const [choosePhoto, setChoosePhoto] = useState('https://apod.nasa.gov/apod/image/1708/Carina_Foucher_960.jpg');
+	const [text, setText] = useState('Press Pin For New Background Photo');
+	const [font, setFont] = useState('Alfa Slab One, cursive');
 
 	// Nasa Query
 	const getNasaPhotos = async ({ queryKey: { 1: dates } }) => {
@@ -26,8 +30,38 @@ function App() {
 	// LOADING => While Loading nasaPhotos Loading will be true
 	if (nasaPhotosLoading) {
 		return (
-			<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '80vh' }}>
-				<Spinner color="info" />
+			<div className="App">
+				<Container>
+					<h1 className="backgroundImageText" style={{ backgroundImage: `url(${choosePhoto})`, fontFamily: font, overflow: 'hidden' }}>
+						{text}
+					</h1>
+					<div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+						<Button style={{ fontFamily: 'Alfa Slab One, cursive' }} color={font === 'Alfa Slab One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Alfa Slab One, cursive')}>
+							Choose Font!
+						</Button>
+						<Button
+							style={{ fontFamily: 'Cinzel Decorative, cursive' }}
+							color={font === 'Cinzel Decorative, cursive' ? 'success' : 'secondary'}
+							onClick={() => setFont('Cinzel Decorative, cursive')}
+						>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Black Ops One, cursive' }} color={font === 'Black Ops One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Black Ops One, cursive')}>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Monoton, cursive' }} color={font === 'Monoton, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Monoton, cursive')}>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Titan One, cursive' }} color={font === 'Titan One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Titan One, cursive')}>
+							Choose Font!
+						</Button>
+					</div>
+					<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '80vh' }}>
+						<Spinner type="grow" color="primary" style={{ width: '100px', height: '100px' }}>
+							.
+						</Spinner>
+					</div>
+				</Container>
 			</div>
 		);
 	}
@@ -40,21 +74,75 @@ function App() {
 		setEndDate(e.target.value);
 	};
 
+	const handleSetText = (e) => {
+		setText(e.target.value);
+	};
+
 	return (
-		<div className="App">
-			<input type="date" value={startDate} onChange={handleStartDate} />
-			<input type="date" value={endDate} onChange={handleEndDate} />
-			<Container>
-				<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-					{nasaPhotos
-						? nasaPhotos.map((photo) => {
+		<ToastProvider autoDismiss autoDismissTimeout={2000} placement="bottom-center">
+			<div className="App">
+				<Container>
+					<h1 className="backgroundImageText" style={{ backgroundImage: `url(${choosePhoto})`, fontFamily: font, overflow: 'hidden' }}>
+						{text} sdfs
+					</h1>
+					<div style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+						<Button style={{ fontFamily: 'Alfa Slab One, cursive' }} color={font === 'Alfa Slab One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Alfa Slab One, cursive')}>
+							Choose Font!
+						</Button>
+						<Button
+							style={{ fontFamily: 'Cinzel Decorative, cursive' }}
+							color={font === 'Cinzel Decorative, cursive' ? 'success' : 'secondary'}
+							onClick={() => setFont('Cinzel Decorative, cursive')}
+						>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Black Ops One, cursive' }} color={font === 'Black Ops One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Black Ops One, cursive')}>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Monoton, cursive' }} color={font === 'Monoton, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Monoton, cursive')}>
+							Choose Font!
+						</Button>
+						<Button style={{ fontFamily: 'Titan One, cursive' }} color={font === 'Titan One, cursive' ? 'success' : 'secondary'} onClick={() => setFont('Titan One, cursive')}>
+							Choose Font!
+						</Button>
+					</div>
+					<div>
+						<div style={{ display: 'flex', justifyContent: 'center', gap: 10, flexWrap: 'wrap', marginBlock: '3em' }}>
+							<FormGroup>
+								<Label className="label" for="input1">
+									Change Text For New Title
+								</Label>
+								<Input type="text" value={text} onChange={handleSetText} id="input1" />
+							</FormGroup>
+							<FormGroup>
+								<Label className="label" for="input2">
+									Photo Taken After
+								</Label>
+								<Input type="date" value={startDate} onChange={handleStartDate} id="input2" id="input1" />
+							</FormGroup>
+							<FormGroup>
+								<Label className="label" for="input3">
+									Photo Taken Before
+								</Label>
+								<Input type="date" value={endDate} onChange={handleEndDate} id="input3" />
+							</FormGroup>
+						</div>
+					</div>
+				</Container>
+				<Container>
+					<div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'space-between' }}>
+						{nasaPhotos ? (
+							nasaPhotos.map((photo) => {
 								const { title, explanation, url, date } = photo;
-								return <PictureCard title={title} explanation={explanation} url={url} date={date} />;
-						  })
-						: ' No Photos Available'}
-				</div>
-			</Container>
-		</div>
+								return <PictureCard setChoosePhoto={setChoosePhoto} title={title} explanation={explanation} url={url} date={date} />;
+							})
+						) : (
+							<div style={{ height: '50vh', color: 'white' }}> No Photos Available'</div>
+						)}
+					</div>
+				</Container>
+			</div>
+		</ToastProvider>
 	);
 }
 
--- src/components/PictureCard.js
+++ src/components/PictureCard.js
@@ -1,41 +1,96 @@
 import { memo, useState } from 'react';
 import { IoHeartSharp, IoHeartOutline } from 'react-icons/io5';
-import { AiOutlineShareAlt } from 'react-icons/ai';
+import { AiFillPushpin, AiOutlinePushpin } from 'react-icons/ai';
+import { HiOutlineShare, HiShare } from 'react-icons/hi';
+import { useToasts } from 'react-toast-notifications';
 
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { motion } from 'framer-motion';
+import useWindowDimensions from '../hooks/windowDimensions';
 
-const PictureCard = memo(({ title, explanation, url, date }) => {
+const PictureCard = memo(({ title, explanation, url, date, setChoosePhoto }) => {
 	const [seeMore, setSeeMore] = useState(false);
+	const [pinPhoto, setPinPhoto] = useState(false);
 	const [pictureLiked, setPictureLiked] = useState(false);
-	console.log(url);
+	const [sharedLink, setSharedLink] = useState(false);
+	const { height, width } = useWindowDimensions();
+	const { addToast } = useToasts();
+
 	const handleSeeMore = () => {
 		setSeeMore(!seeMore);
 		console.log(seeMore);
 	};
 
+	const handlePinning = () => {
+		let pinningText, appearance;
+		if (!pinPhoto) {
+			setChoosePhoto(url);
+			setPinPhoto(true);
+			pinningText = `Pinned Photo - ${title}`;
+			appearance = `success`;
+		} else {
+			setPinPhoto(false);
+			pinningText = `Unpinned Photo - ${title}`;
+			appearance = `error`;
+		}
+
+		addToast(pinningText, {
+			appearance: appearance,
+			autoDismiss: true,
+		});
+	};
+
 	const handleLiking = () => {
 		setPictureLiked(!pictureLiked);
+
+		const likingText = !pictureLiked ? `Liked - ${title}` : `Unliked - ${title}`;
+		const appearance = !pictureLiked ? `success` : `error`;
+
+		addToast(likingText, {
+			appearance: appearance,
+			autoDismiss: true,
+		});
 	};
-	const marginInline = { marginInline: 5 };
+
+	const handleSharing = () => {
+		setSharedLink(true);
+		navigator.clipboard.writeText(url);
+		addToast(`Copied Link - ${title}`, {
+			appearance: 'info',
+			autoDismiss: true,
+		});
+
+		setTimeout(() => {
+			setSharedLink(false);
+		}, 1000);
+	};
 
 	return (
-		<Card className="card1">
-			<CardImg top style={{ objectFit: 'fill', width: '100%', height: '300px' }} src={url.includes('youtube') || url.includes('vimeo') ? '/no_photo_available.png' : url} alt="Card image cap" />
-			<CardBody>
-				<CardTitle tag="h5" style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: seeMore ? '' : 'nowrap' }}>
-					{title}
-				</CardTitle>
-				<CardSubtitle tag="h6" className="mb-2 text-muted">
-					{date}
-				</CardSubtitle>
-				<CardText className={seeMore ? '' : 'card-text-hidden'}>{explanation}</CardText>
-				<div style={{ marginTop: 10, display: 'flex', justifyContent: 'space-around', marginRight: 10 }}>
-					{seeMore ? <Button onClick={handleSeeMore}>Less ?</Button> : <Button onClick={handleSeeMore}>More?</Button>}
-					{pictureLiked ? <IoHeartSharp onClick={handleLiking} size={'2.0em'} color={'red'} /> : <IoHeartOutline onClick={handleLiking} size={'2.0em'} color={'red'} />}
-					<AiOutlineShareAlt size={'2.0em'} color={'#007bff'} />
-				</div>
-			</CardBody>
-		</Card>
+		<motion.div layout data-isOpen={seeMore} className="parent card card1">
+			<div style={width > 700 && seeMore ? { display: 'flex' } : {}}>
+				<CardImg
+					top
+					style={seeMore ? { maxHeight: height - 100 } : { objectFit: 'fill', width: '100%', height: '300px' }}
+					src={url.includes('youtube') || url.includes('vimeo') ? '/no_photo_available.png' : url}
+					alt="Card image cap"
+				/>
+				<CardBody>
+					<CardTitle tag="h5" style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: seeMore ? '' : 'nowrap' }}>
+						{title}
+					</CardTitle>
+					<CardSubtitle tag="h6" className="mb-2 text-muted">
+						{date}
+					</CardSubtitle>
+					<CardText className={seeMore ? '' : 'card-text-hidden'}>{explanation}</CardText>
+					<div style={{ marginTop: 10, display: 'flex', justifyContent: 'space-around', marginRight: 10 }}>
+						{seeMore ? <Button onClick={handleSeeMore}>Less ?</Button> : <Button onClick={handleSeeMore}>More?</Button>}
+						{pinPhoto ? <AiFillPushpin onClick={handlePinning} size={'2.0em'} color={'#28A744'} /> : <AiOutlinePushpin onClick={handlePinning} size={'2.0em'} color={'#28A744'} />}
+						{pictureLiked ? <IoHeartSharp onClick={handleLiking} size={'2.0em'} color={'red'} /> : <IoHeartOutline onClick={handleLiking} size={'2.0em'} color={'red'} />}
+						{sharedLink ? <HiShare size={'2.0em'} color={'#007bff'} /> : <HiOutlineShare onClick={handleSharing} size={'2.0em'} color={'#007bff'} />}
+					</div>
+				</CardBody>
+			</div>
+		</motion.div>
 	);
 });
 
